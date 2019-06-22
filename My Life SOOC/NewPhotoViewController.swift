e9fe985f04c4ae61f63dc0ff6f6d58d70fed4ebe
Ashanti Williams <ashanti.a.williams@hotmail.com>
2019-06-21 22:11:49 -0600

    Iteration 0.5a aka I'm committing early

---
+++ My Life SOOC/NewPhotoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewPhotoViewController.swift
+//  My Life SOOC
+//
+//  Created by Ashanti Williams on 6/21/19.
+//  Copyright © 2019 Ashanti Williams. All rights reserved.
+//
+
+import UIKit
+
+class NewPhotoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
