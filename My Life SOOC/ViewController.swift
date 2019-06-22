0087f266175b6c103a94fb850dd3fb1f7dc07beb
Ashanti Williams <ashanti.a.williams@hotmail.com>
2019-06-21 20:48:14 -0600

    Initial Commit

---
+++ My Life SOOC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My Life SOOC
+//
+//  Created by Ashanti Williams on 6/21/19.
+//  Copyright © 2019 Ashanti Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
